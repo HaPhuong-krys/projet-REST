b174051655e6a52dac28dd98a63c4e92ccf10b9a
Haphuong-krys <haphuong141198@gmail.com>
2023-03-21 16:22:44 +0100

    user

--- src/rest/asii/dao/UserDao.java
+++ src/rest/asii/dao/UserDao.java
@@ -9,15 +9,9 @@ import rest.asii.model.Categorie;
 public enum UserDao {
 	instance;
 
-    private Map<String, Article> articles = new HashMap<>();
-
     private UserDao() {
         User user1 = new User("nom1", "prenom1", "adress1" );
-        User
-
+        User user2 = new User("nom2", "prenom2", "adress2" );
     }
     
-    public Map<String, Article> getModel(){
-        return articles;
-    }
 }
